0d10482bbefcd4bdc923575eadccfa43be8c8999
GhostDev <159514284+AlsoGhostglowDev@users.noreply.github.com>
2024-08-11 00:39:19 +0800

    oc robot templates

---
+++ robot_script_templates/ustairs.lua
@@ -0,0 +1,34 @@
+local r = require 'robot'
+r.setLightColor(0x888888)
+
+local function loopFunc(f, n) 
+    for i = 1, n do f(i) end
+end
+
+local function main()
+    r.turnRight()
+    loopFunc(function(_)
+        r.swing()
+        r.swingDown()
+        r.down()
+    end, 3)
+    r.swing()
+    r.turnLeft()
+    loopFunc(r.up, 2)
+end
+
+main()
+
+local i = -1
+while true do
+    i = i + 1
+    if (i % 8 == 0) then
+        r.turnRight()
+        r.place()
+        r.turnLeft()
+    end
+    r.swing()
+    r.forward()
+
+    main()
+end
